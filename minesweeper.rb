51b2e3b37f4b025ea323d4cf2dee71250eb59fb0
Mike & Leah <Mike+Leah@appacademy.io>
2015-08-03 18:18:43 -0400

    Enable file load + save + exit

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,11 +4,48 @@ require 'json'
 require_relative 'board'
 
 class Minesweeper
+  def self.start_up
+    input = ""
+    puts "Are you loading an old game file (Y/N)"
+    until self.is_valid_choice?(input)
+      input = gets.chomp.upcase
+      puts "Please choose Y or N" unless self.is_valid_choice?(input)
+    end
+
+    case input
+    when "Y"
+      self.load
+    when "N"
+      Minesweeper.new(9,10).play
+    end
+  end
+
+  def self.load
+    puts "What local file would you like to load? "
+    filename = gets.chomp
+    contents = File.read(filename)
+    saved_game = YAML.load(contents)
+    saved_game.play
+  end
+
+  def self.is_valid_choice?(choice)
+    ["Y","N"].include?(choice)
+  end
+
   attr_reader :board
 
-  def initialize(size = 9, mine_num = 10)
-    @board = Board.new(size,mine_num).populate_grid
+  def initialize(size = 9, bomb_num = 10)
+    @board = Board.new(size,bomb_num).populate_grid
   end
+
+  def save
+    puts "What would you like to name your file (Ex:game1.txt) ?"
+    filename = gets.chomp
+    File.open(filename,"w") do |file|
+      file.puts self.to_yaml
+    end
+  end
+
 
   def play
     board.render
@@ -23,6 +60,9 @@ class Minesweeper
 
   def take_turn
     act = self.action
+
+    save if act == "S"
+    Process.exit!(true) if act == "X"
     pos = self.position
 
     case act
@@ -61,7 +101,7 @@ class Minesweeper
 
   private
   def is_valid_action?(string)
-    ["R","F"].include?(string)
+    ["R","F","S","X"].include?(string)
   end
 
   def is_valid_pos?(array)
@@ -75,7 +115,6 @@ class Minesweeper
 end
 
 if __FILE__ == $PROGRAM_NAME
-  game = Minesweeper.new(4,16)
-  game.play
+  Minesweeper.start_up
 
 end
