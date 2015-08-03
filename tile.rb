370898e40d7dffc34dc37c56cde81be9fcee3511
Mike & Leah <Mike+Leah@appacademy.io>
2015-08-03 11:54:07 -0400

    Add Tile#initialize.

--- tile.rb
+++ tile.rb
@@ -3,4 +3,14 @@ require 'json'
 require 'yaml'
 
 class Tile
+  attr_reader :is_bomb, :pos, :board
+  attr_accessor :flagged, :revealed
+
+  def initialize(is_bomb, pos, board)
+    @is_bomb = is_bomb
+    @pos = pos
+    @board = board
+    @flagged = false
+    @revealed = false
+  end
 end
