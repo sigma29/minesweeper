c46dc09b14b85ca85d31b6b6fcd5415762d4116d
Mike & Leah <Mike+Leah@appacademy.io>
2015-08-03 12:16:36 -0400

    Create syntactic sugar for Board.grid

--- board.rb
+++ board.rb
@@ -4,11 +4,22 @@ require 'yaml'
 require_relative 'tile'
 
 class Board
-  attr_reader :size, :num_mines
+  attr_reader :size, :num_mines, :grid
 
   def initialize(size = 9,num_mines = 10)
     @size = size
     @num_mines = num_mines
+    @grid = Array.new(size) { Array.new(size) }
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos,value)
+    row,col = pos
+    @grid[row][col] = value
   end
 
 end
